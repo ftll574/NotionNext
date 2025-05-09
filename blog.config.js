387b04f58e28c7bbbb4fde3f2bd824b6ec03fcef
zhenyu <wheal609@gmail.com>
2025-05-10 01:03:41 +0800

    New SEO

--- blog.config.js
+++ blog.config.js
@@ -5,6 +5,7 @@ const BLOG = {
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
     '183b75929a5e802e886cd7d8291e7804',
+  TITLE: process.env.NEXT_PUBLIC_TITLE || '鑫葳貿易有限公司：專業塑膠原料供應商', // 網站標題，用於SEO和瀏覽器標籤
   THEME: process.env.NEXT_PUBLIC_THEME || 'starter', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-TW', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
